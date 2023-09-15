a4e580d943e1d90c5f95474d04ab35e9fbd7aade
Andrei Shpakovskiy <shpakovskiyandrei@gmail.com>
2023-09-15 13:43:08 +0300

    Adjust status bar content color to selected theme

--- app/src/main/java/com/shpak/stormalert/presentation/ui/theme/Theme.kt
+++ app/src/main/java/com/shpak/stormalert/presentation/ui/theme/Theme.kt
@@ -64,7 +64,7 @@ fun StormAlertTheme(
         SideEffect {
             val window = (view.context as Activity).window
             window.statusBarColor = colorScheme.background.toArgb()
-            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
+            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = !darkTheme
         }
     }
 
