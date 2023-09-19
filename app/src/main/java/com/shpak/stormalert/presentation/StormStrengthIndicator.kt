c7c6600e2c0bac327e9c449eb45607de718c00cb
Andrei Shpakovskiy <shpakovskiyandrei@gmail.com>
2023-09-19 10:21:13 +0300

    Change yellow color tone

--- app/src/main/java/com/shpak/stormalert/presentation/StormStrengthIndicator.kt
+++ app/src/main/java/com/shpak/stormalert/presentation/StormStrengthIndicator.kt
@@ -11,7 +11,7 @@ import androidx.compose.ui.unit.dp
 fun StormStrengthIndicator(strength: Double) {
     val color = when {
         strength < 4.0 -> Color(0xFF1FFC50)
-        strength >= 4 && strength < 5 -> Color(0xFFFFFA3D)
+        strength >= 4 && strength < 5 -> Color(0xFFFFE700)
         strength >= 5 && strength < 6 -> Color(0xFFFF801E)
         strength >= 6 && strength < 7 -> Color(0xFFFE4512)
         else -> Color(0xFFFF0004)
