2c192ef816284674d979b471c62d8ba06ad3e5bb
Andrei Shpakovskiy <50966785+AndreiShpakovskiy@users.noreply.github.com>
2024-05-12 12:25:28 +0300

    Try new settings UI

--- app/src/main/java/com/shpak/stormalert/presentation/settings/SettingsUi.kt
+++ app/src/main/java/com/shpak/stormalert/presentation/settings/SettingsUi.kt
@@ -1,25 +1,35 @@
 package com.shpak.stormalert.presentation.settings
 
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.ripple.rememberRipple
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.shpak.stormalert.R
 import com.shpak.stormalert.presentation.ui.theme.StormAlertTheme
 
@@ -62,15 +72,40 @@ private fun SettingsScreen(
             .padding(innerPadding)
             .padding(16.dp)
     ) {
-        SettingsGroup(R.string.settings_group_notifications) {
-            SettingsSwitch(
-                icon = R.drawable.ic_launcher_foreground,
-                iconDesc = R.string.error_forecast_fetch_failed,
-                name = R.string.app_name,
-                state = viewModel.areNotificationsEnabled.collectAsState()
+        Column {
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier.clickable(
+                    interactionSource = MutableInteractionSource(),
+                    indication = rememberRipple(color = Color.Black),
+                    onClick = {},
+                )
             ) {
-                viewModel.toggleNotificationsSwitch()
+                Column(modifier = Modifier.weight(1.0f)) {
+                    Text(text = "Storm ahead", fontSize = 22.sp)
+                    Text(
+                        text = "Storm with a Kp index >= N is forecasted to occur in the near future",
+                        fontSize = 14.sp,
+                        lineHeight = 16.sp,
+                        modifier = Modifier.alpha(0.8f)
+                    )
+                }
+
+                Switch(
+                    checked = true,
+                    onCheckedChange = {}
+                )
             }
+
+            Text(
+                text = "Choose Kp threshold",
+                color = MaterialTheme.colorScheme.primary,
+                fontSize = 14.sp,
+                fontWeight = FontWeight(600),
+                modifier = Modifier
+                    .padding(vertical = 8.dp)
+                    .clickable { }
+            )
         }
     }
 }
