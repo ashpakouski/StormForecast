e0b3e4201090b4d664047083f4249effad18c056
Andrei Shpakovskiy <50966785+AndreiShpakovskiy@users.noreply.github.com>
2024-05-24 20:51:39 +0300

    Fix navigation bar padding

--- app/src/main/java/com/shpak/stormalert/presentation/forecast/ForecastListUi.kt
+++ app/src/main/java/com/shpak/stormalert/presentation/forecast/ForecastListUi.kt
@@ -3,8 +3,11 @@ package com.shpak.stormalert.presentation.forecast
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.navigationBars
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
@@ -23,6 +26,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
@@ -101,7 +105,7 @@ private fun ForecastLoadingSucceededState(forecast: GeomagneticForecast?) {
         }
 
         item {
-            Box(modifier = Modifier.height(16.dp))
+            NavigationBarSpacer()
         }
     }
 }
@@ -124,4 +128,16 @@ private fun ForecastLoadingFailedState(modifier: Modifier) {
             modifier = Modifier.padding(32.dp)
         )
     }
+}
+
+@Composable
+private fun NavigationBarSpacer() {
+    val density = LocalDensity.current
+    val navigationBarHeight = with(density) {
+        WindowInsets.navigationBars.getBottom(this).toDp()
+    }.let {
+        if (it.value > 0.0f) it else 16.dp
+    }
+
+    Spacer(modifier = Modifier.height(navigationBarHeight))
 }
